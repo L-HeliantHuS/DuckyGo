be11bb1f52dfca27ab7f63f11035d00d4d44416b
HeliantHuS <1984441370@qq.com>
2019-09-16 16:22:13 +0800

    Change Cors, Release mode enable cors, so debug mode cros is disabled.

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,7 +10,12 @@ func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie"}
-	config.AllowOrigins = []string{"http://www.example.com"}
+	// 运行再Release模式下才会进行跨域保护 保证开发过程中不会被跨域困扰~
+	if gin.Mode() == gin.ReleaseMode {
+		config.AllowOrigins = []string{"http://www.example.com"}
+	} else {
+		config.AllowAllOrigins = true
+	}
 	config.AllowCredentials = true
 	return cors.New(config)
 }
