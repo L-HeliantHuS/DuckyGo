05616929f19b80974dcbce69e0c1a8c6f8c41cff
HeliantHuS <1984441370@qq.com>
2019-09-19 13:59:18 +0800

    Delete log middleware

--- server/router.go
+++ server/router.go
@@ -15,10 +15,6 @@ func NewRouter() *gin.Engine {
 	r.StaticFile("/favicon.ico", "static/favicon.ico")
 
 	// 中间件, 顺序不能改
-	// 这条是为了防止因为记录日志而导致性能损失
-	if os.Getenv("LOG_LEVEL") == "DEBUG" {
-		r.Use(middleware.SaveLog())
-	}
 	r.Use(middleware.SessionCookie(os.Getenv("SESSION_SECRET")))
 	r.Use(middleware.Cors())
 	r.Use(middleware.CurrentUser())
