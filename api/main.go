2105e232541faee20d50964ed5a3ec639b3456e2
HeliantHuS <1984441370@qq.com>
2019-09-25 08:35:58 +0800

    Change Status to Code | September 25 2019

--- api/main.go
+++ api/main.go
@@ -46,23 +46,23 @@ func ErrorResponse(err error) serializer.Response {
 			field := conf.T(fmt.Sprintf("Field.%s", e.Field))
 			tag := conf.T(fmt.Sprintf("Tag.Valid.%s", e.Tag))
 			return serializer.Response{
-				Status: 40001,
-				Msg:    fmt.Sprintf("%s%s", field, tag),
-				Error:  fmt.Sprint(err),
+				Code:  40001,
+				Msg:   fmt.Sprintf("%s%s", field, tag),
+				Error: fmt.Sprint(err),
 			}
 		}
 	}
 	if _, ok := err.(*json.UnmarshalTypeError); ok {
 		return serializer.Response{
-			Status: 40001,
-			Msg:    "JSON类型不匹配",
-			Error:  fmt.Sprint(err),
+			Code:  40001,
+			Msg:   "JSON类型不匹配",
+			Error: fmt.Sprint(err),
 		}
 	}
 
 	return serializer.Response{
-		Status: 40001,
-		Msg:    "参数错误",
-		Error:  fmt.Sprint(err),
+		Code:  40001,
+		Msg:   "参数错误",
+		Error: fmt.Sprint(err),
 	}
 }
